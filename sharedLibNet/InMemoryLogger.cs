f3a16537f7bd5d22c1d79b2e338f58e3aa840a60
Joscha Metze <joscha.metze@hochfrequenz.de>
2018-08-16 18:28:35 +0200

    add logger classes

---
+++ sharedLibNet/InMemoryLogger.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Extensions.Logging;
+using sharedLibNet.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sharedLibNet
+{
+    /// <summary>
+    /// A logger that writes messages to a an in-memory list.
+    /// Courtesy of https://github.com/StephenClearyApps/DotNetApis
+    /// </summary>
+    public sealed class InMemoryLoggerProvider : ILoggerProvider
+    {
+        private readonly List<LogMessage> _messages = new List<LogMessage>();
+
+        public List<LogMessage> Messages
+        {
+            get { lock (_messages) return _messages.ToList(); }
+        }
+
+        void IDisposable.Dispose() { }
+
+        public ILogger CreateLogger(string categoryName) => new InMemoryLogger(this, categoryName);
+
+        private void Log<TState>(string categoryName, LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            var message = new LogMessage
+            {
+                Type = logLevel,
+                Timestamp = DateTimeOffset.UtcNow,
+                Message = formatter(state, exception) + ( exception == null ? "" : "\r\n" + exception ),
+                Category = categoryName,
+                EventId = eventId.Id,
+            };
+
+            lock (_messages)
+                _messages.Add(message);
+        }
+
+        private sealed class InMemoryLogger : ILogger
+        {
+            private readonly InMemoryLoggerProvider _provider;
+            private readonly string _categoryName;
+
+            public InMemoryLogger(InMemoryLoggerProvider provider, string categoryName)
+            {
+                _provider = provider;
+                _categoryName = categoryName;
+            }
+
+            public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+                => _provider.Log(_categoryName, logLevel, eventId, state, exception, formatter);
+
+            public bool IsEnabled(LogLevel logLevel) => true;
+
+            public IDisposable BeginScope<TState>(TState state) => throw new NotImplementedException();
+        }
+    }
+}
---
+++ sharedLibNet/Logging/LogMessage.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sharedLibNet.Logging
+{
+    /// Courtesy of https://github.com/StephenClearyApps/DotNetApis
+    public sealed class LogMessage
+    {
+        public LogLevel Type { get; set; }
+
+        [JsonConverter(typeof(EpochTimestampJsonConverter))]
+        public DateTimeOffset Timestamp { get; set; }
+
+        public string Message { get; set; }
+
+        public string Category { get; set; }
+
+        public int EventId { get; set; }
+    }
+}
