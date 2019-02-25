70951574ddfb71ceea85330545aed2afb2bdea5e
Konstantin KLein <konstantin.klein@hochfrequenz.de>
2019-02-25 09:58:40 +0100

    Unittest for encrypted logobjects

--- sharedLibNet/LoggerHelper.cs
+++ sharedLibNet/LoggerHelper.cs
@@ -2,7 +2,7 @@
 using System.Dynamic;
 using System.Net.Http;
 using System.Threading.Tasks;
-using BO4E.BO.LogObject;
+using BO4E.BO;
 using BO4E.Extensions.Encryption;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
@@ -16,6 +16,19 @@ namespace sharedLibNet
         public static HttpClient httpClient = new HttpClient();
 
         /// <summary>
+        /// Creates a new logger and logger provider from a newly instantiated LoggerFactory.
+        /// </summary>
+        /// <param name="serviceName">name of the service. Is passed to the logger provider.</param>
+        /// <returns>Tuple of logger and corresponding loggerProvider</returns>
+        public static (ILogger logger, InMemoryLoggerProvider loggerProvider) CreateLogger(string serviceName)
+        {
+            var factory = new LoggerFactory();
+            var loggerProvider = new InMemoryLoggerProvider();
+            factory.AddProvider(loggerProvider);
+            return (factory.CreateLogger(serviceName), loggerProvider);
+        }
+
+        /// <summary>
         /// Creates log message; allows for encryption/obfuscation of sensitive information.
         /// </summary>
         /// <param name="content">log message itself</param>
@@ -24,14 +37,13 @@ namespace sharedLibNet
         /// <returns></returns>
         public static string CreateTraceObject(string content, bool sensitive = false, string publicKey = null)
         {
-            //TODO: enable encryption of sensitive information
             dynamic obj = new ExpandoObject();
             obj.Sensitive = sensitive;
             if (sensitive)
             {
                 if (publicKey == null)
                 {
-                    throw new ArgumentNullException("To encrypt sensitive data you need to provide a non null public key");
+                    throw new ArgumentNullException("To encrypt sensitive data you have to provide a non-null public key");
                 }
                 byte[] publicKeyBytes;
                 try
@@ -40,14 +52,16 @@ namespace sharedLibNet
                 }
                 catch (FormatException e)
                 {
-                    throw new FormatException($"The public key string {publicKey} you provided is no valid base64 string: {e.Message}");
+                    throw new FormatException($"The provided public key string '{publicKey}' is no valid base64 string: {e.Message}");
                 }
                 KeyPair asykeyPairSender = PublicKeyBox.GenerateKeyPair(); // newly generated every single time???
                 AsymmetricEncrypter enc = new AsymmetricEncrypter(asykeyPairSender);
-                LogObject lo = new LogObject();
-                lo.datetime = DateTime.UtcNow;
-                lo.logMessage = content;
-                obj.Content = enc.Encrypt(lo, publicKey);
+                LogObject logObject = new LogObject
+                {
+                    datetime = DateTime.UtcNow,
+                    logMessage = content
+                };
+                obj.Content = enc.Encrypt(logObject, publicKey);
             }
             else
             {
@@ -55,26 +69,23 @@ namespace sharedLibNet
             }
             return JsonConvert.SerializeObject(obj);
         }
-        public static (ILogger logger, InMemoryLoggerProvider loggerProvider) CreateLogger(string serviceName)
-        {
-            var factory = new LoggerFactory();
-            var loggerProvider = new InMemoryLoggerProvider();
-            factory.AddProvider(loggerProvider);
-            return (factory.CreateLogger(serviceName), loggerProvider);
-        }
+
+        [Obsolete("Please use SendToLogServer(Uri, ...) instead of SendToLogServer(string, ...).")]
         public static async Task<HttpResponseMessage> SendLogToServer(string URL, InMemoryLoggerProvider logger, string certificate)
         {
-            var cert = certificate;
-            if (httpClient.DefaultRequestHeaders.Contains(CustomHeader.XArrClientCert))
-            {
-                httpClient.DefaultRequestHeaders.Remove(CustomHeader.XArrClientCert);
-            }
-
-            httpClient.DefaultRequestHeaders.Add(CustomHeader.XArrClientCert, cert);
-
-            return await httpClient.PostAsync(URL, new StringContent(JsonConvert.SerializeObject(logger.Messages)));
+            Uri uri = new Uri(URL);
+            return await SendLogToServer(uri, logger, certificate);
         }
-        public static async Task<HttpResponseMessage> GetEventLogs(string URL, string certificate)
+
+
+        /// <summary>
+        /// POSTs messages of <paramref name="logger"/> to server specified in <paramref name="URL"/>.  
+        /// </summary>
+        /// <param name="URL">Uri of the server</param>
+        /// <param name="logger">logger containing messages</param>
+        /// <param name="certificate">certificate used in HTTP POST header</param>
+        /// <returns>HttpClient response of POST</returns>
+        public static async Task<HttpResponseMessage> SendLogToServer(Uri URL, InMemoryLoggerProvider logger, string certificate)
         {
             var cert = certificate;
             if (httpClient.DefaultRequestHeaders.Contains(CustomHeader.XArrClientCert))
@@ -82,7 +93,30 @@ namespace sharedLibNet
                 httpClient.DefaultRequestHeaders.Remove(CustomHeader.XArrClientCert);
             }
             httpClient.DefaultRequestHeaders.Add(CustomHeader.XArrClientCert, cert);
+            return await httpClient.PostAsync(URL, new StringContent(JsonConvert.SerializeObject(logger.Messages)));
+        }
 
+        [Obsolete("Please use GetEventLogs(Uri, ...) instead of GetEventLogs(string, ...).")]
+        public static async Task<HttpResponseMessage> GetEventLogs(string URL, string certificate)
+        {
+            Uri uri = new Uri(URL);
+            return await GetEventLogs(uri, certificate);
+        }
+
+        /// <summary>
+        /// Reads events logs from server.
+        /// </summary>
+        /// <param name="URL">URL of server the log messages are retrieved from</param>
+        /// <param name="certificate">certificate used in HTTP GET header</param>
+        /// <returns>HttpClient response of GET</returns>
+        public static async Task<HttpResponseMessage> GetEventLogs(Uri URL, string certificate)
+        {
+            var cert = certificate;
+            if (httpClient.DefaultRequestHeaders.Contains(CustomHeader.XArrClientCert))
+            {
+                httpClient.DefaultRequestHeaders.Remove(CustomHeader.XArrClientCert);
+            }
+            httpClient.DefaultRequestHeaders.Add(CustomHeader.XArrClientCert, cert);
             return await httpClient.GetAsync(URL);
         }
     }
