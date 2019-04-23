da16c4b2d4e23b79d4ef493171535bd35668f7d3
Joscha Metze <joscha.metze@hochfrequenz.de>
2019-04-23 20:43:47 +0200

    add parameter to CoreModule

--- sharedLibNet/DependencyInjection/CoreAppModule.cs
+++ sharedLibNet/DependencyInjection/CoreAppModule.cs
@@ -12,13 +12,18 @@ namespace sharedLibNet.DependencyInjection
 {
     public class CoreAppModule : Module
     {
+        protected string _authServiceName;
+        public CoreAppModule(string authServiceName)
+        {
+            this._authServiceName = authServiceName;
+        }
         public override void Load(IServiceCollection services)
         {
             var config = new ConfigurationBuilder()
                     .AddJsonFile("local.settings.json", optional: true, reloadOnChange: false)
                     .AddEnvironmentVariables()
                     .Build();
-            var _authHelper = new AuthenticationHelper("MasterProcessData", config[EnvironmentVariableNames.ENV_AUTH_URL], config);
+            var _authHelper = new AuthenticationHelper(_authServiceName, config[EnvironmentVariableNames.ENV_AUTH_URL], config);
             var _raygun = new RaygunClient(config[EnvironmentVariableNames.ENV_RAYGUN_API_KEY]);
             services.AddSingleton<IAuthenticationHelper>(_authHelper);
             services.AddSingleton(_raygun);
