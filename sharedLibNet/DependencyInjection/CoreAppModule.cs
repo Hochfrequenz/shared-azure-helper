ef3bc29e831c6f9d689d6f085082024b74c5ad03
Joscha Metze <joscha.metze@hochfrequenz.de>
2019-05-31 15:30:59 +0200

    add azure app configuration

--- sharedLibNet/DependencyInjection/CoreAppModule.cs
+++ sharedLibNet/DependencyInjection/CoreAppModule.cs
@@ -1,5 +1,8 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
 using Microsoft.Extensions.DependencyInjection;
 using Mindscape.Raygun4Net.AspNetCore;
 using sharedLibNet.Helper;
@@ -20,8 +23,21 @@ namespace sharedLibNet.DependencyInjection
             var config = new ConfigurationBuilder()
                     .AddJsonFile(_configName, optional: true, reloadOnChange: false)
                     .AddEnvironmentVariables()
+                    .AddAzureAppConfiguration(options =>
+                    {
+                        options.Connect(Environment.GetEnvironmentVariable("AzureAppConfigConnectionString"))
+                               .Use(KeyFilter.Any, LabelFilter.Null)
+                               .Use(KeyFilter.Any, Environment.GetEnvironmentVariable("Environment"));
+                    })
                     .Build();
-            var _authHelper = new AuthenticationHelper(_authServiceName, config[EnvironmentVariableNames.ENV_AUTH_URL], config);
+            var _config = new AuthConfiguration()
+            {
+                ApiKey = config[EnvironmentVariableNames.ENV_HF_API_KEY],
+                AuthURL = config[EnvironmentVariableNames.ENV_AUTH_URL],
+                Issuers = config.GetSection("ISSUERS")?.Get<string[]>(),
+                Audiences = config.GetSection("AUDIENCES")?.Get<string[]>()
+            };
+            var _authHelper = new AuthenticationHelper(_authServiceName, config[EnvironmentVariableNames.ENV_AUTH_URL], _config);
             var _raygun = new RaygunClient(config[EnvironmentVariableNames.ENV_RAYGUN_API_KEY]);
             services.AddSingleton<IAuthenticationHelper>(_authHelper);
             services.AddSingleton<IConfiguration>(config);
--- sharedLibNet/Helper/EnvironmentalVariableNames.cs
+++ sharedLibNet/Helper/EnvironmentalVariableNames.cs
@@ -5,52 +5,42 @@
         /// <summary>
         /// name of the environment variable containing the RayGun API key
         /// </summary>
-        public const string ENV_RAYGUN_API_KEY = "RAYGUN_API";
+        public const string ENV_RAYGUN_API_KEY = "Raygun:ApiKey";
 
         /// <summary>
         /// name of the environment variable containing the Hochfrequenz API Key
         /// </summary>
-        public const string ENV_HF_API_KEY = "API_KEY";
+        public const string ENV_HF_API_KEY = "ApiGateway:Key";
 
         /// <summary>
         /// name of the environment variable containing the URL of the authentication service
         /// </summary>
-        public const string ENV_AUTH_URL = "AUTH_URL";
-
-        /// <summary>
-        /// name of the environment variable containing the URL of the logging service
-        /// </summary>
-        public const string ENV_LOGGING_URL = "LOGGING_URL";
+        public const string ENV_AUTH_URL = "CoreServices:URL";
 
         /// <summary>
         /// name of the environment variable containing the URL of the lookup service
         /// </summary>
-        public const string ENV_LOOKUP_URL = "LOOKUP_URL";
-
-        /// <summary>
-        /// name of the environment variable containing the URL of the lookup service
-        /// </summary>
-        public const string ENV_SERVICE_URL = "SERVICE_URL";
+        public const string ENV_SERVICE_URL = "EnergyFunctions:URL";
 
         /// <summary>
         /// name of environment variable containing the URL of Azure Key Vault
         /// </summary>
         /// <seealso cref="ENV_KEYVAULT_CLIENT"/>
         /// <seealso cref="ENV_KEYVAULT_SECRET"/>
-        public const string ENV_KEYVAULT_URL = "KeyVaultURL";
+        public const string ENV_KEYVAULT_URL = "KeyVault:URL";
 
         /// <summary>
         /// name of environment variable containing the KeyVault Client Id
         /// </summary>
         /// <seealso cref="ENV_KEYVAULT_SECRET"/>
         /// <seealso cref="ENV_KEYVAULT_URL"/>
-        public const string ENV_KEYVAULT_CLIENT = "KeyVaultAppClientId";
+        public const string ENV_KEYVAULT_CLIENT = "KeyVault:ClientId";
 
         /// <summary>
         /// name of the environment variable containing the KeyVault Client Secret
         /// </summary>
         /// <seealso cref="ENV_KEYVAULT_CLIENT"/>
         /// <seealso cref="ENV_KEYVAULT_URL"/>
-        public const string ENV_KEYVAULT_SECRET = "KeyVaultAppClientSecret";
+        public const string ENV_KEYVAULT_SECRET = "KeyVault:ClientSecret";
     }
 }
